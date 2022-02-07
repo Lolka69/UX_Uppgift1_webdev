c88c30d0caf70a6da92fa545552a30e7428e82e6
Lolita Tochiieva <lolita.tochiieva@chasacademy.se>
2022-02-07 15:47:26 +0100

    changed to png, figma illustrations bad in SVG

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 function phoneMenu() {
+    // Grab the mobile_links div with query selector
     var menuLinks = document.querySelector(".mobile_links");
+    //Debug ouput to make sure the correct element is grabbed
     console.log(menuLinks)
+
+    //Checks if mobile links are displayed or not, if displayed, change to none
     if (menuLinks.style.display === 'block') {
         menuLinks.style.display = 'none';
         console.log("pressed?")
     } else {
+        // If the mobile links are not display, change to displayed
         console.log("pressing?")
         menuLinks.style.display = 'block'
     }
